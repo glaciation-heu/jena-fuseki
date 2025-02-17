62c19198eb043139d16d50d44d15d220fb7f87c1
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2025-02-17 11:36:28 +0100

    increasing max form content size limit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,8 @@
 echo "Copying default shiro.ini to PVC..."
 cp /jena-fuseki/shiro.ini /fuseki/shiro.ini
 
+# Increase Jetty's max form content size limit
+export JVM_ARGS="${JVM_ARGS} -Dorg.eclipse.jetty.server.Request.maxFormContentSize=10000000"
+
 # Start Fuseki
 exec /sbin/tini -- /jena-fuseki/fuseki-server
