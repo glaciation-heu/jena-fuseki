c7744d9fe670eff3ef68c1e1386b82430ac2facf
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2025-02-19 12:35:54 +0100

    restart jena fuseki regularly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -20,5 +20,24 @@ fi
 export JVM_ARGS="${JVM_ARGS} -Xmx2048m -Xms2048m -Dorg.eclipse.jetty.server.Request.maxFormKeys=100000"
 echo "JVM_ARGS: $JVM_ARGS"
 
-# Start Fuseki
-exec /sbin/tini -- /jena-fuseki/fuseki-server
+# Function to start Fuseki
+start_fuseki() {
+    echo "Starting Jena Fuseki..."
+    /sbin/tini -- /jena-fuseki/fuseki-server &
+    FUSEKI_PID=$!
+}
+
+# Start Fuseki for the first time
+start_fuseki
+
+# Periodically restart Fuseki
+while true; do
+    sleep 2100  # Restart every 35 minutes (adjust as needed)
+    
+    echo "Stopping Jena Fuseki..."
+    kill $FUSEKI_PID
+    wait $FUSEKI_PID  # Ensure the process has fully stopped
+    
+    echo "Restarting Jena Fuseki..."
+    start_fuseki
+done
