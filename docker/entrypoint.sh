118e66b955e8ba11778f778e7f9167f9f04b3f31
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2025-03-03 12:55:11 +0100

    cleaning up old Data-* folders in slice

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+SLICE_DIR="$FUSEKI_BASE/databases/slice"
+
 # Ensure shiro.ini exists
 echo "Copying default shiro.ini to PVC..."
 cp /jena-fuseki/shiro.ini /fuseki/shiro.ini
 
 # Ensure required directories exist
 mkdir -p $FUSEKI_BASE/configuration
-mkdir -p $FUSEKI_BASE/databases/slice
+mkdir -p $SLICE_DIR
 
 # Move dataset configuration if not already present
 if [ ! -f "$FUSEKI_BASE/configuration/slice.ttl" ]; then
@@ -27,6 +29,21 @@ start_fuseki() {
     FUSEKI_PID=$!
 }
 
+cleanup_old_data() {
+    local dir="$1"
+
+    # Get the latest Data-* folder
+    local latest=$(ls -d $dir/Data-* 2>/dev/null | egrep -v "\-tmp\$" | sort -V | tail -n 1)
+
+    if [[ -n "$latest" ]]; then
+        # Remove all except the latest one
+        ls -d $dir/Data-* 2>/dev/null | grep -v "^$latest$" | xargs rm -rf --
+        echo "Kept: $latest, deleted others."
+    else
+        echo "No Data-* folders found in '$dir'."
+    fi
+}
+
 # Start Fuseki for the first time
 start_fuseki
 
@@ -37,6 +54,9 @@ while true; do
     echo "Stopping Jena Fuseki..."
     kill $FUSEKI_PID
     wait $FUSEKI_PID  # Ensure the process has fully stopped
+
+    echo "Cleaning up old $SLICE_DIR/Data-* foders..."
+    cleanup_old_data "$SLICE_DIR"
     
     echo "Restarting Jena Fuseki..."
     start_fuseki
